363391baa4bd2f1478d0182004b576da055305ac
root <root@ip-172-31-41-69.ap-south-1.compute.internal>
2024-06-07 05:10:09 +0000

    inital commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_ecs_cluster" "example" {
+  name = "example-cluster"
+}
+
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "YOUR_DOCKER_IMAGE_URI"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "example" {
+  name            = "example-service"
+  cluster         = aws_ecs_cluster.example.id
+  task_definition = aws_ecs_task_definition.example.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-0c84aad6df33abf0f"]
+    security_groups = ["sg-02afe6bebd72a7d86"]
+  }
+}
+
